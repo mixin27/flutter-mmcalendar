d63a1d30c09e59daae43f671c48139afbbedaf22
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-09-28 17:03:52 +0630

    add constant values for Algorithm calculations

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,16 @@
+/// Constants value for Algorithm calculations
+class Constants {
+  /// Solar year - `(365.2587565) (1577917828 / 4320000.0)`
+  static const double solarYear = 365.2587564814815;
+
+  /// Lunar month - `(29.53058795) (1577917828 / 53433336.0)`
+  static const double lunarMonth = 29.53058794607172;
+
+  /// Beginning of 0 ME - `Myanmar era`
+  ///
+  /// Starting year of Myanmar era (year 0)
+  static const double mo = 1954168.050623;
+
+  /// Beginning of `3rd Era`
+  static const double se3 = 29.53058794607172;
+}
